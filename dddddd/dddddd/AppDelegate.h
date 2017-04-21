71dd09ae26f5274ad08e31370ae52c92f8b3ae25
sChao001 <948994708@qq.com>
2017-04-21 11:57:36 +0800

    clone for commit

---
+++ dddddd/dddddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dddddd
+//
+//  Created by BCH on 2017/4/21.
+//  Copyright © 2017年 BCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dddddd/dddddd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dddddd
+//
+//  Created by BCH on 2017/4/21.
+//  Copyright © 2017年 BCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
